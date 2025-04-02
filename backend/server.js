710ef0c9b57d29102dfcdfb9c4c3ea818c50e579
Anu@1027 <anuradhasomisetti1027@gmail.com>
2025-04-02 15:24:42 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -81,8 +81,8 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
-
 
